77440d1af5d4b4beb666c56f6078a3b64c69dfd1
botamochi-dev <239866373+botamochi-dev@users.noreply.github.com>
2026-05-15 14:46:55 +0900

    fix: コードの整形とインクルード順序を修正

--- main/app.h
+++ main/app.h
@@ -2,15 +2,14 @@
 extern "C" {
 #endif
 
-
 #include <kernel.h>
 
 /* タスク優先度 */
-#define MAIN_PRIORITY    5 /* メインタスク */
-#define TRACER_PRIORITY  6 /* ライントレースタスク */
+#define MAIN_PRIORITY 5   /* メインタスク */
+#define TRACER_PRIORITY 6 /* ライントレースタスク */
 
 #ifndef STACK_SIZE
-#define STACK_SIZE      (4096)
+#define STACK_SIZE (4096)
 #endif /* STACK_SIZE */
 
 #ifndef TOPPERS_MACRO_ONLY
