28b255e912bf0b10c940981a7d5454f44aa1f76f
botamochi-dev <239866373+botamochi-dev@users.noreply.github.com>
2026-07-02 07:44:43 +0000

    style: auto format with clang-format

--- main/app/FormatTestTmp.cpp
+++ main/app/FormatTestTmp.cpp
@@ -1,30 +1,25 @@
-class FormatTestTmp
-{
+class FormatTestTmp {
 public:
     FormatTestTmp(int value);
     int getValue();
     void setValue(int v);
+
 private:
     int value;
 };
 
-FormatTestTmp::FormatTestTmp( int value )
-{
-	this->value = value;
+FormatTestTmp::FormatTestTmp(int value) {
+    this->value = value;
 }
 
-int FormatTestTmp::getValue( )
-{
-    if(value>0){
-		return value;
-	}
-	else
-	{
-        return   0;
-	}
+int FormatTestTmp::getValue() {
+    if(value > 0) {
+        return value;
+    } else {
+        return 0;
+    }
 }
 
-void FormatTestTmp::setValue(int   v)
-{
-	value=v;
+void FormatTestTmp::setValue(int v) {
+    value = v;
 }
